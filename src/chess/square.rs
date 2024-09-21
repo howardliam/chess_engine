9fe9080d358f9a5d20430a0338e29572d2d06103
Liam Howard <lima.mail725@gmail.com>
2024-09-22 00:41:30 +1000

    Changed Square struct into a tuple struct.

--- src/chess/square.rs
+++ src/chess/square.rs
@@ -13,18 +13,16 @@ impl Coord {
 }
 
 #[derive(Clone, Copy)]
-pub struct Square {
-    pub square: usize,
-}
+pub struct Square(pub usize);
 
 impl Square {
     pub fn new(square: usize) -> Self {
-        Self { square }
+        Self(square)
     }
 
     pub fn to_coord(&self) -> Coord {
-        let file = (self.square & 0b111) as i32;
-        let rank = ((self.square >> 3) & 0b111) as i32;
+        let file = (self.0 & 0b111) as i32;
+        let rank = ((self.0 >> 3) & 0b111) as i32;
 
         Coord::new(file, rank)
     }
@@ -42,7 +40,7 @@ impl Add<i32> for Square {
     type Output = Self;
 
     fn add(self, rhs: i32) -> Self::Output {
-        Square::new(self.square + rhs as usize)
+        Square::new(self.0 + rhs as usize)
     }
 }
 
@@ -50,18 +48,18 @@ impl Sub<i32> for Square {
     type Output = Self;
 
     fn sub(self, rhs: i32) -> Self::Output {
-        Square::new(self.square - rhs as usize)
+        Square::new(self.0 - rhs as usize)
     }
 }
 
 impl AddAssign<i32> for Square {
     fn add_assign(&mut self, rhs: i32) {
-        self.square += rhs as usize;
+        self.0 += rhs as usize;
     }
 }
 
 impl SubAssign<i32> for Square {
     fn sub_assign(&mut self, rhs: i32) {
-        self.square -= rhs as usize;
+        self.0 -= rhs as usize;
     }
 }
